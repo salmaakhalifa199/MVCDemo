bc585a979997674f078678a1c64cb903d31d270a
salmaakhalifa199 <salmakhalifa06991@gmail.com>
2024-08-28 01:30:52 +0300

    Add project files.

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           return View(); 
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+        //public ActionResult Index()
+        //{
+        //    //ContentResult contentResult = new ContentResult();
+        //    //contentResult.Content = "Hello from content result";
+        //    //return contentResult;
+
+        //    return Content("Hello from content"); //helper
+        //}
+
+
+        //public ActionResult AboutUs()
+        //{
+        //    return Redirect("https://drive.google.com/drive/u/1/folders/18B9-kvGFWuqql0Ai_iRunjecAYvr4F-G");
+        //    //return RedirectToAction("Index");
+        //}
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+        // fi .net 8 34an astghdam el ConfigureServices bktab b3d builder fi el program net8
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // fi .net 8 34an astghdam el Configure bktab b3d app fi program  net8
+            app.UseAuthentication(); // el tarteb moham authentication abl el authorize tb3n 
+            app.UseAuthorization();
+            app.UseRouting();
+
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+        }
+    }
+}
